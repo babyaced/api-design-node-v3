c93e46094267d0dfb044130ff825f862b6abe537
babyaced <dannyr.simpson@gmail.com>
2021-06-25 09:24:45 -0700

    lesson 1 done

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
-
+const router = express.Router()
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +12,59 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  req.mydata = 'hello'
+  next()
+}
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+//cats
+const routes = [
+  'get /cat',
+  'get /cat/:id',
+  'post /cat',
+  'put /cat/:id',
+  'delete /cat/:id'
+]
+
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+app.get('^(me)', (req, res) => {
+  res.send({ message: 'me' })
+})
+
+app.get('/app/*', (req, res) => {
+  res.send({ app: 'app' })
+})
+
+app.get('/data', [log, log, log], (req, res) => {
+  res.send({ data: req.mydata })
+})
+
+app.post('/data', log, (req, res) => {
+  console.log(req.body)
+  res.send({ ok: true })
+})
+
+app.put('/data', (req, res) => {})
+
+app.delete('/data', (req, res) => {})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}

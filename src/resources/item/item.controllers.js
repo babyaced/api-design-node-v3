f89302db6b50d713eb56c98433d8b23cfc1a1073
babyaced <dannyr.simpson@gmail.com>
2021-06-25 13:10:57 -0700

    lesson 3 done

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,26 @@
+import mongoose from 'mongoose'
 import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
 
-export default crudControllers(Item)
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  const updated = await Item.findByIdAndUpdate(
+    item._id,
+    { name: 'eat' },
+    { new: true }
+  ).exec()
+
+  const removed = await Item.findByIdAndRemove(item._id).exec()
+  console.log(removed)
+}
+// export default crudControllers(Item)
+
+run()

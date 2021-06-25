4145fc11816207c6b41d5390c285b447697f7405
babyaced <dannyr.simpson@gmail.com>
2021-06-25 09:49:56 -0700

    lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,34 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .post((req, res) => {
+    res.send(req.body)
+  })
+  .get((req, res) => {
+    res.send({ data: 'data' })
+  })
+  .put((req, res) => {
+    res.send({ data: 'updatedData' })
+  })
+  .delete((req, res) => {
+    res.send({ deleted: true })
+  })
+
+router
+  .route('/:id')
+  .post((req, res) => {
+    res.send(req.body)
+  })
+  .get((req, res) => {
+    res.send({ data: 'data' })
+  })
+  .put((req, res) => {
+    res.send({ data: 'updatedData' })
+  })
+  .delete((req, res) => {
+    res.send({ deleted: true })
+  })
+
 export default router
